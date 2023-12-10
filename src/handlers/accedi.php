742d22e05be6490b581dd0dc67d6f3196d3e6889
archy <archy@yeet.sus>
2023-12-10 13:34:01 +0100

    Reindirizzamento alla pagina di accesso con gli errori riscontrati

--- src/handlers/accedi.php
+++ src/handlers/accedi.php
@@ -1,13 +1,15 @@
 <?php
 require_once('../data/database.php');
 try {
+  set_error_handler(function ($severity, $message, $file, $line) {
+    throw new \ErrorException($message, $severity, $severity, $file, $line);
+  });
+
+  session_start();
   [
     "mail" => $mail,
     "password" => $password,
   ] = $_POST;
-  if ($mail == null || $password == null) {
-    throw new Exception("Dati \$_POST invalidi");
-  }
 
   $query =  "SELECT mail,status FROM Users WHERE mail = '@MAIL' AND password = '@PASSWORD' LIMIT 1;";
   $args = [
@@ -16,20 +18,14 @@ try {
   ];
   $res = Database::connect_execute_clean($query, $args);
   if (count($res) == 0) {
-    throw new Exception("Nessun utente trovato");
+    throw new Exception("Nessun utente trovato. Le credenziali potrebbero essere errate.");
   }
 
-  if (session_start() === false) {
-    throw new Exception("Errore durante l'avvio della sessione");
-  }
   $user = $res[0];
-  $_SESSION = $user;
+  $_SESSION['user'] = $user;
+  //TODO reindirizzamento a una pagina più appropriata
   header("Location: /");
 } catch (Exception $e) {
-  // TODO sostituire con il reindirazzamento a una pagina di errore
-  echo "<a href='./esci.php'>clicca per fare il sign out</a>";
-  echo "<p>dump dell'eccezione</p>";
-  echo "<textarea style='width:90%; height:90%;' readonly>";
-  var_dump($e);
-  echo "</textarea>";
+  $_SESSION['loginErrors'] = $e;
+  header("Location: /accedi.php");
 }

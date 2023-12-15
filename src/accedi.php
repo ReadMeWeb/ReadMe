228228a03d1c62e043542ae429019195f780bbe4
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2023-12-15 23:06:16 +0100

    Accedi e Registrati sostiuiscono i placeholder in layout.html

--- src/accedi.php
+++ src/accedi.php
@@ -1,4 +1,6 @@
 <?php
+require_once 'components/navbar.php';
+
 function gethandlererror($name) {
   if (array_key_exists($name, $_SESSION)) {
     $e = $_SESSION[$name];
@@ -23,6 +25,17 @@ if (session_start()) {
   }
 
   //TODO ripristinare la mail / password all'interno degli input
+
+  //TODO prevenire gli <a> ricorsivi / ad anello
+  //soluzione temporanea
+  $menu = navbar();
+  $menu = str_replace("href='accedi.php'","",$menu);
+
+  $page = str_replace("{{title}}", "Accedi", $page);
+  $page = str_replace("{{description}}", "Pagina di accesso di Orchestra", $page);
+  $page = str_replace("{{keywords}}", "Orchestra, musica classica, accesso, log in, sign in", $page);
+  $page = str_replace("{{menu}}", $menu, $page);
+  $page = str_replace("{{breadcrumbs}}", "<a href='./'>Home</a> &gt <a>Accedi</a>", $page);
 
   $page = str_replace("{{content}}", $content, $page);
   $page = str_replace("{{errori}}", $errori, $page);
--- src/registrati.php
+++ src/registrati.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'components/navbar.php';
 
 function gethandlererror($name) {
   if (array_key_exists($name, $_SESSION)) {
@@ -15,7 +16,7 @@ if (session_start()) {
   }
 
   //TODO sostituire con il file_get_contents di layout
-  $page = "<!DOCTYPE html><html><body>{{content}}</body></html>";
+  $page = file_get_contents("./components/layout.html");
   $content = file_get_contents("./components/registrati.html");
   $errori = "";
   if ($e = gethandlererror('signupErrors')) {
@@ -24,6 +25,17 @@ if (session_start()) {
   }
 
   //TODO ripristinare nome utente / mail / password all'interno degli input
+
+  //TODO prevenire gli <a> ricorsivi / ad anello
+  //soluzione temporanea
+  $menu = navbar();
+  $menu = str_replace("href='registrati.php'","",$menu);
+
+  $page = str_replace("{{title}}", "Registrati", $page);
+  $page = str_replace("{{description}}", "Pagina di registrazione di Orchestra", $page);
+  $page = str_replace("{{keywords}}", "Orchestra, musica classica, registrazione, sign up", $page);
+  $page = str_replace("{{menu}}", $menu, $page);
+  $page = str_replace("{{breadcrumbs}}", "<a href='./'>Home</a> &gt <a>Registrati</a>", $page);
 
   $page = str_replace("{{content}}", $content, $page);
   $page = str_replace("{{errori}}", $errori, $page);

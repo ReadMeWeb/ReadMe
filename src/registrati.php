6c2de45b1e4c86641bb60bc176b29748a7f87e45
lumine <hakupisi>
2024-01-06 14:02:52 +0100

    spostato l'handler di registrati in registrati.php

--- src/registrati.php
+++ src/registrati.php
@@ -1,10 +1,65 @@
 <?php
+
+if (count($_POST) == 0) {
+  goto get;
+}
+
+// ========================================================================================================================
+// POST
+// ========================================================================================================================
+
+require_once 'data/database.php';
+require_once "components/sessionEstablisher.php";
+
+try {
+  set_error_handler(function ($severity, $message, $file, $line) {
+    throw new \ErrorException($message, $severity, $severity, $file, $line);
+  });
+
+  try_session();
+  [
+    "username" => $username,
+    "password" => $password,
+  ] = $_POST;
+
+
+  $conn = new Database();
+  if ($conn->user_exists($username)) {
+    throw new Exception("Il Nome utente fornito risulta già registrato.");
+  }
+
+  if ($conn->user_sign_up($username, $password) !== true) {
+    // Questo caso non dovrebbe mai succedere
+    throw new Exception("Errore del database.");
+  }
+  $conn->close();
+
+  //NOT TODO reindirizzamento a una pagina più appropriata <- delegato a handlers/accedi.php
+  //TODO reindirizzamento a handlers/accedi.php con accesso automatico
+  $_SESSION['user'] = ['username' => $username, 'status' => "USER"];
+  header("Location: /");
+} catch (Exception $e) {
+  //var_dump($e);
+  $_SESSION['signupErrors'] = $e;
+  header("Location: /registrati.php");
+}
+
+if(count($_POST) > 0){
+  exit();
+}
+
+// ========================================================================================================================
+// GET
+// ========================================================================================================================
+get:
+
 require_once 'components/navbar.php';
 require_once 'components/sessionEstablisher.php';
 require_once 'components/breadcrumbs/breadcrumbItem.php';
 require_once 'components/breadcrumbs/breadcrumbsBuilder.php';
 
-function gethandlererror($name) {
+function gethandlererror($name)
+{
   if (array_key_exists($name, $_SESSION)) {
     $e = $_SESSION[$name];
     unset($_SESSION[$name]);

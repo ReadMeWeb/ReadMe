3fb1f67c22b537aa0bfb76594976d24a49a5d0fa
lumine <hakupisi>
2024-02-28 14:19:47 +0100

    aggiornato accedi per funzionare con pangine

--- src/Pages/accedi.php
+++ src/Pages/accedi.php
@@ -1,4 +1,8 @@
 <?php
+
+use Pangine\Pangine;
+
+require_once '../Pangine/Pangine.php';
 require_once '../Pangine/HTMLBuilder.php';
 require_once '../components/breadcrumbs/breadcrumbItem.php';
 require_once '../components/breadcrumbs/breadcrumbsBuilder.php';
@@ -6,6 +10,9 @@ require_once '../components/navbar.php';
 require_once '../components/sessionEstablisher.php';
 require_once '../data/database.php';
 require_once '../handlers/utils.php';
+
+const assert = new AssertionFacade();
+assert->is_noone_signed_in();
 
 set_error_handler(function ($severity, $message, $file, $line) {
   throw new \ErrorException($message, $severity, $severity, $file, $line);
@@ -19,52 +26,55 @@ if (is_user_signed_in() || is_admin_signed_in()) {
   redirect('/');
 }
 
-$errori = '';
-$nome = '';
+const logerr = 'logerr';
 
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-  goto GET;
-}
+(new Pangine())
+  ->POST_read(function () {
+    $nome = '';
 
-// ========================================================================================================================
-// POST
-// ========================================================================================================================
+    try {
+      [
+        "name" => $nome,
+        "password" => $password,
+      ] = $_POST;
 
-try {
-  [
-    "name" => $nome,
-    "password" => $password,
-  ] = $_POST;
+      $conn = new Database();
+      $res = $conn->user_with_mail_password($nome, $password);
+      $conn->close();
+      if (count($res) == 0) {
+        throw new Exception("Nessun utente trovato. Le credenziali potrebbero essere errate.");
+      }
 
-  $conn = new Database();
-  $res = $conn->user_with_mail_password($nome, $password);
-  $conn->close();
-  if (count($res) == 0) {
-    throw new Exception("Nessun utente trovato. Le credenziali potrebbero essere errate.");
-  }
+      $_SESSION['user'] = $res[0];
+      redirect(extract_from_array_else('redirection', $_SESSION, '/'));
+    } catch (Exception $e) {
+      $_SESSION[logerr] = ['nome' => $nome, 'val' => $e->getMessage(), 'typ' => HTMLBuilder::ERROR_P];
+      //ritorno alla pagina di accesso in caso di errore
+      //in commentato perché se no va in redirect loop
+      //redirect('accedi.php');
+    }
+    echo '<p>è entrato in post, nonostante la chiamata sia get</p>';
+  })
+  ->GET_read(function () {
 
-  $_SESSION['user'] = $res[0];
-  redirect(extract_from_array_else('redirection', $_SESSION, '/'));
-} catch (Exception $e) {
-  $errori = $e->getMessage();
-}
+    if (!array_key_exists(logerr, $_SESSION)) {
+      $_SESSION[logerr] = ['nome' => '', 'val' => '', 'typ' => HTMLBuilder::UNSAFE];
+    }
 
-// ========================================================================================================================
-GET:
-// ========================================================================================================================
-
-echo (new HTMLBuilder('../components/layout.html'))
-  ->set('title', 'Accedi')
-  ->set('description', 'Pagina di accesso di Orchestra')
-  ->set('keywords', 'Orchestra, musica classica, accesso, log in, sign in')
-  ->set('menu', navbar())
-  ->set('breadcrumbs', (new BreadcrumbsBuilder())
-    ->addBreadcrumb(new BreadcrumbItem("Home"))
-    ->addBreadcrumb(new BreadcrumbItem("Accedi", isCurrent: true))
-    ->build()
-    ->getBreadcrumbsHtml())
-  ->set('content', (new HTMLBuilder('../components/accedi.html'))
-    ->set('nome', $nome)
-    ->set('errori', $errori, HTMLBuilder::ERROR_P)
-    ->build())
-  ->build();
+    echo (new HTMLBuilder('../components/layout.html'))
+      ->set('title', 'Accedi')
+      ->set('description', 'Pagina di accesso di Orchestra')
+      ->set('keywords', 'Orchestra, musica classica, accesso, log in, sign in')
+      ->set('menu', navbar())
+      ->set('breadcrumbs', (new BreadcrumbsBuilder())
+        ->addBreadcrumb(new BreadcrumbItem("Home"))
+        ->addBreadcrumb(new BreadcrumbItem("Accedi", isCurrent: true))
+        ->build()
+        ->getBreadcrumbsHtml())
+      ->set('content', (new HTMLBuilder('../components/accedi.html'))
+        ->set('nome', $_SESSION[logerr]['nome'])
+        ->set('errori', $_SESSION[logerr]['err'], $_SESSION[logerr]['typ'])
+        ->build())
+      ->build();
+  })
+  ->execute();

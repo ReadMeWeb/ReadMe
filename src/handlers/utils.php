4c7fc14883c211bd90773a6fef48eeeb296ad094
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-03-07 09:46:44 +0100

    dbcall

--- src/handlers/utils.php
+++ src/handlers/utils.php
@@ -1,35 +1,32 @@
 <?php
 
-function redirect($url): void
-{
+require_once '../data/database.php';
+
+function redirect($url): void {
   header('Location: ' . $url);
   exit();
 }
 
-function is_not_signed_in(): bool
-{
+function is_not_signed_in(): bool {
   return $_SESSION['user']['status'] === 'UNREGISTERED';
 }
 
-function is_user_signed_in(): bool
-{
+function is_user_signed_in(): bool {
   return $_SESSION['user']['status'] === 'USER';
 }
 
-function is_admin_signed_in(): bool
-{
+function is_admin_signed_in(): bool {
   return $_SESSION['user']['status'] === 'ADMIN';
 }
 
-function extract_from_array_else($key, $array, $otherwise)
-{
+function extract_from_array_else($key, $array, $otherwise) {
   $result = array_key_exists($key, $array) ? $array[$key] : $otherwise;
   unset($array[$key]);
   return $result;
 }
 
 function upload_file(string $dest_dir, string $uploaded_file_path, string $uploaded_file_name): string {
-  if(!is_dir($dest_dir)) {
+  if (!is_dir($dest_dir)) {
     mkdir($dest_dir);
   }
   $file_ext = pathinfo($uploaded_file_name, PATHINFO_EXTENSION);
@@ -38,3 +35,15 @@ function upload_file(string $dest_dir, string $uploaded_file_path, string $uploa
   return $file_final_name;
 }
 
+function dbcall($function) {
+  $db = null;
+  try {
+    $db = new Database();
+    return $function($db);
+  } finally {
+    match ($db) {
+      null => null,
+      default => $db->close(),
+    };
+  }
+}

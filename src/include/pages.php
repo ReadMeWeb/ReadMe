6cb297a766662a15db91d31ba52e8259ce53e260
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-03-09 12:08:39 +0100

    rimosso il path

--- src/include/pages.php
+++ src/include/pages.php
@@ -1,23 +1,21 @@
 <?php
 
-const path = '/';
-
 const pages = [
-        'Home' =>  path . '/index.php',
-        'Chi Siamo' =>  path . '/chisiamo.php',
-        'Accedi' =>  path . '/Pages/accedi.php',
-        'Registrati' => path . '/Pages/registrati.php',
-        'Catalogo' =>  path . '/Pages/catalogo.php',
-        'Aggiungi Artista' =>  path . '/Pages/artista.php?create=true',
-        'Modifica Artista' =>  path . '/Pages/artista.php?update=true',
-        'Albums' =>  path . '/Pages/albums.php',
-        'Crea album' =>  path . '/Pages/album.php?create=true',
+        'Home' => 'index.php',
+        'Chi Siamo' => 'chisiamo.php',
+        'Accedi' => 'Pages/accedi.php',
+        'Registrati' =>'Pages/registrati.php',
+        'Catalogo' => 'Pages/catalogo.php',
+        'Aggiungi Artista' => 'Pages/artista.php?create=true',
+        'Modifica Artista' => 'Pages/artista.php?update=true',
+        'Albums' => 'Pages/albums.php',
+        'Crea album' => 'Pages/album.php?create=true',
         'Ispeziona album' => '',
         'Modifica album' => '',
-        'Aggiungi Canzone' =>  path . '/selectartist.php',
-        'Informazioni Brano' =>  path . '/addsong.php',
-        'Canzone Aggiunta' =>  path . '/successaddsong.php',
-        'Account' =>  path . '/Pages/account.php',
-        'Account (Modifica)' =>  path . '/Pages/account?update=true.php',
-        'Permessi insufficienti' =>  path . '/Pages/unallowed.php'
+        'Aggiungi Canzone' => 'selectartist.php',
+        'Informazioni Brano' => 'addsong.php',
+        'Canzone Aggiunta' => 'successaddsong.php',
+        'Account' => 'Pages/account.php',
+        'Account (Modifica)' => 'Pages/account?update=true.php',
+        'Permessi insufficienti' => 'Pages/unallowed.php'
     ];

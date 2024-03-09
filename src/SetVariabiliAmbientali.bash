c864e79d102b25d7226009f3b4f278c4636264a4
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-03-09 13:38:49 +0100

    aggiunto uno script di adattamento da docker al server tecweb

---
+++ src/SetVariabiliAmbientali.bash
@@ -0,0 +1,26 @@
+function parsepath () {
+        sed 's/\//\\\//g' <<< $*
+}
+
+function setincludepath() {
+        find . \
+                -type f \
+                -name '*.php' \
+                -exec sed -i 's/set_include_path(.*);/set_include_path('"'$(parsepath $*)'"');/' '{}'  \;
+}
+
+function setpagepath(){
+        sed -i 's/const path = .*;/const path = '"'$(parsepath $*)'"';/' include/pages.php
+}
+
+function setmysqllogin(){
+        sed -i "s/private const HOST = '.*';/private const HOST = 'localhost';/" include/database.php
+        sed -i "s/private const USER_NAME = '.*';/private const USER_NAME = '$1';/" include/database.php
+        sed -i "s/private const PASSWORD = '.*';/private const PASSWORD = '$2';/" include/database.php
+        sed -i "s/private const DATABASE = '.*';/private const DATABASE = '$1';/" include/database.php
+}
+
+setincludepath ''         # esempio: '/home/< VOSTO NOME UTENTE >/public_html/orchestra/'
+setpagepath ''            # esempio: '/< VOSTO NOME UTENTE >/orchestra/'
+setmysqllogin '' '' '' '' # esempio: '< VOSTO NOME UTENTE >' '< LA VOSTRA PASSWORD DI MYSQL >'
+

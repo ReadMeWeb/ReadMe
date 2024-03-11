9b832f326059379f0b2c4c6e1bdd85228777a5c1
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-03-11 22:18:22 +0100

    exit fa redirect su home

--- src/Pages/exit.php
+++ src/Pages/exit.php
@@ -1,10 +1,15 @@
 <?php
+
 set_include_path($_SERVER['DOCUMENT_ROOT']);
 require_once 'include/sessionEstablisher.php';
+require_once 'include/include.php';
+require_once 'include/pages.php';
+
 try_session();
 $status = $_SESSION['user']['status'];
-if($status = "USER" || $status == "ADMIN"){
+if($status == "USER" || $status == "ADMIN"){
     unset($_SESSION['user']);
     session_destroy();
 }
-header("Location: /index.php");
+
+redirect(pages['Home']);

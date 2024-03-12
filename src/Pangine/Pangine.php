e52bc33084b8952ebd751e712680ac9b6ae1fef1
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-03-12 15:25:58 +0100

    correzione import

--- src/Pangine/Pangine.php
+++ src/Pangine/Pangine.php
@@ -5,7 +5,7 @@ namespace Pangine;
 set_include_path($_SERVER['DOCUMENT_ROOT']);
 require_once 'include/sessionEstablisher.php';
 require_once 'include/HTMLBuilder.php';
-require_once 'include/include.php';
+require_once 'include/utils.php';
 require_once 'include/pages.php';
 
 class Pangine

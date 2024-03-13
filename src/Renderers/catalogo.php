c968d5d980748d5e69e361930db0593327474c60
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-03-13 10:53:25 +0100

    uso dell'operatore ternario

--- src/Renderers/catalogo.php
+++ src/Renderers/catalogo.php
@@ -18,15 +18,11 @@ function isSequencePresent(string $haystack, string $sequence) {
 $get_catalogo = function () {
     try_session();
 
-    $layout = "";
-
-    if (is_admin_signed_in() || is_user_signed_in()) {
-        $layout = new HTMLBuilder("../components/layoutLogged.html");
-    } else {
-        $layout = (new HTMLBuilder("../components/layout.html"))
+    $layout = (is_admin_signed_in() || is_user_signed_in())
+        ? (new HTMLBuilder("../components/layoutLogged.html"))
+        : ((new HTMLBuilder("../components/layout.html"))
         ->set('description', 'Pagina di catalogo musicale di musica classica di Orchestra')
-        ->set('keywords', 'Orchestra, Catalogo musicale, Musica, Album, Artisti, Canzoni');
-    }
+        ->set('keywords', 'Orchestra, Catalogo musicale, Musica, Album, Artisti, Canzoni'));
 
     $layout
     ->set('title', 'Catalogo')

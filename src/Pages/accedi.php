4dd522f5571348e7fc7e4b6ee4ed156b4cc3faac
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-04-16 10:40:20 +0200

    " -> '

--- src/Pages/accedi.php
+++ src/Pages/accedi.php
@@ -7,6 +7,7 @@ require_once __DIR__ . '/../Utils/ErroriMigliori.php';
 require_once __DIR__ . '/../Utils/Stream.php';
 require_once __DIR__ . '/../Utils/Database.php';
 require_once __DIR__ . '/../Pangine/Pangine.php';
+require_once __DIR__ . '/../Pangine/utils/LayoutBuilder.php';
 
 const registrati = 'signup';
 
@@ -16,12 +17,12 @@ const registrati = 'signup';
   }, needs_database: true)
   ->add_renderer_GET(function () {
     echo (new LayoutBuilder())
-      ->tag_lazy_replace("title", "Accedi")
-      ->tag_lazy_replace("description", "Pagina di accesso alla biblioteca di <span lang='en'>ReadMe</span>")
-      ->tag_lazy_replace("keywords", "ReadMe, biblioteca, libri, narrativa, prenotazioni, accedi")
-      ->tag_lazy_replace("menu", Pangine::navbar_list())
-      ->tag_lazy_replace("breadcrumbs", Pangine::breadcrumbs_generator(array("Home", "Accedi")))
-      ->tag_istant_replace("content", "")
+      ->tag_lazy_replace('title', 'Accedi')
+      ->tag_lazy_replace('description', 'Pagina di accesso alla biblioteca di ReadMe')
+      ->tag_lazy_replace('keywords', 'ReadMe, biblioteca, libri, narrativa, prenotazioni, accedi')
+      ->tag_lazy_replace('menu', Pangine::navbar_list())
+      ->tag_lazy_replace('breadcrumbs', Pangine::breadcrumbs_generator(array('Home', 'Accedi')))
+      ->tag_istant_replace('content', '')
       ->build();
   })
   //registrati

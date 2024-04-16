eac8debf31e6facd9a9f54d1f7f0f446d42322de
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-04-16 18:22:20 +0200

    unregistered non è ammesso a esci.php

--- src/Pangine/Pangine.php
+++ src/Pangine/Pangine.php
@@ -58,7 +58,7 @@ class Pangine
             ),
             "Esci" => array(
               "path" => "/Pages/esci.php",
-              "privileges" => array(self::UNREGISTERED(), self::USER(), self::ADMIN())
+              "privileges" => array(self::USER(), self::ADMIN())
             )
         );
       }

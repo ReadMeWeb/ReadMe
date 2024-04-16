e590a684b75a38ddd7487c40c96d753d5507863c
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-04-16 10:37:03 +0200

    pagina accedi

--- src/Pages/accedi.php
+++ src/Pages/accedi.php
@@ -1,20 +1,32 @@
 <?php
 
 use Pangine\Pangine;
+use \Pangine\utils\LayoutBuilder;
 
+require_once __DIR__ . '/../Utils/ErroriMigliori.php';
 require_once __DIR__ . '/../Utils/Stream.php';
 require_once __DIR__ . '/../Utils/Database.php';
 require_once __DIR__ . '/../Pangine/Pangine.php';
 
+const registrati = 'signup';
+
 (new Pangine())
   //accedi
   ->add_renderer_POST(function () {
-  })
+  }, needs_database: true)
   ->add_renderer_GET(function () {
+    echo (new LayoutBuilder())
+      ->tag_lazy_replace("title", "Accedi")
+      ->tag_lazy_replace("description", "Pagina di accesso alla biblioteca di <span lang='en'>ReadMe</span>")
+      ->tag_lazy_replace("keywords", "ReadMe, biblioteca, libri, narrativa, prenotazioni, accedi")
+      ->tag_lazy_replace("menu", Pangine::navbar_list())
+      ->tag_lazy_replace("breadcrumbs", Pangine::breadcrumbs_generator(array("Home", "Accedi")))
+      ->tag_istant_replace("content", "")
+      ->build();
   })
   //registrati
   ->add_renderer_POST(function () {
-  })
+  }, registrati, true)
   ->add_renderer_GET(function () {
-  })
+  }, registrati, true)
   ->execute();

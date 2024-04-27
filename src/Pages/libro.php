0092ca707ebf8fc18efa1835bbd8a01ae7358724
Matteo Rango <rango.matteo@icloud.com>
2024-04-27 13:58:23 +0200

    Add user type conditional rendering for book page

--- src/Pages/libro.php
+++ src/Pages/libro.php
@@ -16,17 +16,21 @@ use \Utils\Database;
             $book_data = [];
 
             (new Validator("/Pages/404.php"))
-                ->add_parameter("id")->is_numeric(value_parser: function(int $book_id) use ($db, &$book_data) {
-                      $book_data = $db->execute_query("SELECT * FROM Books B INNER JOIN Authors A ON A.id = B.author_id WHERE A.id= ?", $book_id);
-                      if (count($book_data) == 0) {
-                          return "Il libro richiesto non esiste.";
-                      }
-                      return "";
-                })->validate();
+                ->add_parameter("id")
+                ->is_numeric(value_parser: function(int $book_id) use ($db, &$book_data) {
+                    $book_query = "SELECT * FROM Books B INNER JOIN Authors A ON A.id = B.author_id WHERE A.id= ?";
+                    $book_data = $db->execute_query($book_query, $book_id);
+                    if (count($book_data) == 0) {
+                        return "Il libro richiesto non esiste.";
+                    }
+                    return "";
+                })
+                ->validate();
 
             $book_data = $book_data[0];
 
-            echo (new LayoutBuilder())
+            $layout  = (new LayoutBuilder())
+                ->tag_istant_replace("content", $content)
                 ->tag_lazy_replace("title", $book_data["title"])
                 ->tag_lazy_replace("description", $book_data["description"])
                 ->tag_lazy_replace("keywords", "ReadMe, biblioteca, libri, narrativa, prenotazioni")
@@ -35,9 +39,54 @@ use \Utils\Database;
                 ->tag_lazy_replace("book-title", $book_data["title"])
                 ->tag_lazy_replace("book-author", $book_data["name_surname"])
                 ->tag_lazy_replace("book-description", $book_data["description"])
-                ->tag_lazy_replace("cover-image-src", $book_data["cover_file_name"])
-                ->tag_lazy_replace("copies-remaining", $book_data["number_of_copies"])
-                ->tag_istant_replace("content", $content)
+                ->tag_lazy_replace("cover-image-src", $book_data["cover_file_name"]);
+            $operations = "";
+            if($_SESSION["user"]["status"] != Pangine::UNREGISTERED()) {
+                if ($_SESSION["user"]["status"] == Pangine::USER() || $_SESSION["user"]["status"] == Pangine::ADMIN()) {
+                    $remaining_query = "
+                        select id, (number_of_copies - COALESCE(number_of_loans, 0)) as copies_remaining
+
+                        from Books as b
+
+                        left join (
+
+                            select book_id, count(book_id) as number_of_loans
+                            from Loans
+                            where loan_start_date <= now() AND
+                                  loan_expiration_date >= now()
+                            group by book_id
+
+                        ) as l
+
+                        on l.book_id = b.id
+                        where b.id = ?
+                    ";
+                    $remaining = $db->execute_query($remaining_query, $book_data["id"])[0]["copies_remaining"];
+                    $disabled = $remaining == 0 ? "disabled" : "";
+                    $copies = $book_data["number_of_copies"];
+                    $operations .= "
+                        <div id='book_user_op_section'>
+                            <p>Numero di copie possedute: $copies</p>
+                            <form id='book_user_op_form' method='POST' action='/marango/Pages/libro.php'>
+                                <p>Copie rimanenti: $remaining</p>
+                                <input type='submit' name='noleggia' value='Noleggia' $disabled/>
+                            </form>
+                        </div>
+                    ";
+                }
+                if ($_SESSION["user"]["status"] == Pangine::ADMIN()) {
+
+                    $operations .= "
+                            <form id='book_admin_op_form' method='POST' action='/marango/Pages/libro.php'>
+                                <input type='submit' name='modifica' value='Modifica'/>
+                                <input type='submit' name='elimina' value='Elimina'/>
+                            </form>
+                    ";
+                }
+            }
+
+            echo $layout
+                ->tag_lazy_replace("operations", $operations)
                 ->build();
         },
         needs_database: true

db9ba1ce623e6ee256877b9eabcf910bafd73037
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-05-02 14:48:44 +0200

    aggiunto la pagina dei libri

--- src/Pangine/Pangine.php
+++ src/Pangine/Pangine.php
@@ -60,6 +60,10 @@ class Pangine
                 "path" => "/marango/Pages/libro.php",
                 "privileges" => array()
             ),
+            "Libri" => array(
+                "path" => "/marango/Pages/loans.php",
+                "privileges" => array(self::USER())
+            ),
             "Esci" => array(
               "path" => "/marango/Pages/esci.php",
               "privileges" => array(self::USER(), self::ADMIN())

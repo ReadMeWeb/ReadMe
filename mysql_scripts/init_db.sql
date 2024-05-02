d49f299f5eb03490fe025669f01baea69d64d44e
kara <ssctonioloriccardo@gmail.com>
2024-05-02 09:09:01 +0200

    fixed db for retro-compatibility

--- mysql_scripts/init_db.sql
+++ mysql_scripts/init_db.sql
@@ -40,8 +40,8 @@ CREATE TABLE Loans
     CONSTRAINT check_dates CHECK (loan_expiration_date > loan_start_date)
 );
 
-INSERT INTO Users (username,password,status) VALUES ('admin','admin','ADMIN');
-INSERT INTO Users (username,password,status) VALUES ('user','user','USER');
+INSERT INTO Users (username,password,status) VALUES ('admin','8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918','ADMIN');
+INSERT INTO Users (username,password,status) VALUES ('user','04f8996da763b7a969b1028ee3007569eaf3a635486ddab211d512c85b9df8fb','USER');
 
 INSERT INTO Authors (id, name_surname) VALUES (1, 'Fayina');
 INSERT INTO Authors (id, name_surname) VALUES (2, 'Demetri');

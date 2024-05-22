76de65825e3dde35627b11e8ebfa365f0287e1b5
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-05-22 13:05:32 +0200

    rimozione di marango2

--- src/Pages/catalogo.php
+++ src/Pages/catalogo.php
@@ -13,10 +13,10 @@ function get_book_card(string $cover_file_name, string $title, int $copies, stri
 {
     return "<div>
                     <dt>{$title}</dt>
-                    <dd><img src='/marango/assets/book_covers/{$cover_file_name}' alt='' width='200' height='200'></dd>
+                    <dd><img src='assets/book_covers/{$cover_file_name}' alt='' width='200' height='200'></dd>
                     <dd>Autore: {$name_surname}</dd>
                     <dd>Disponibilità: {$copies}</dd>
-                    <dd><a href='/marango/Pages/libro.php?id={$id}'>Visualizza</a></dd>
+                    <dd><a href='Pages/libro.php?id={$id}'>Visualizza</a></dd>
             </div>";
 }
 

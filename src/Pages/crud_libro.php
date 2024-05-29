ceb8c12d7b225bc77b49beb94d40a52e34163848
Pedro-Leonii <pedroleoni2001@gmail.com>
2024-05-29 20:33:56 +0200

    fix: redirect alla pagina di catalogo

--- src/Pages/crud_libro.php
+++ src/Pages/crud_libro.php
@@ -254,7 +254,7 @@ use Pangine\utils\Validator;
     $book_data = $db->execute_query($book_query, $_POST['id']);
     if(count($book_data) != 1){
         Pangine::set_general_message("Non è stato possbile trovare il libro richiesto, riprovare (ERR_BOOK_07)");
-        		Pangine::redirect("Pages/catalogo.php");
+        		Pangine::redirect("Pages/catalogo.php?page=1&query=");
     }
     $file_name = $book_data[0]["cover_file_name"];
 
@@ -277,6 +277,6 @@ use Pangine\utils\Validator;
 
     $db->get_connection()->commit();
     Pangine::set_general_message("Libro eliminato con successo","success");
-    		Pangine::redirect("Pages/catalogo.php");
+    		Pangine::redirect("Pages/catalogo.php?page=1&query=");
 }, "elimina", needs_database: true)
 ->execute();
